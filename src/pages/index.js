d8302b69fe3a4c1b9a8d01a85e6ed9d6ca154b47
HaLan0101 <dnhalan0101@gmail.com>
2022-03-21 02:22:12 +0100

    save code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import LogoNetFlix from "../public/LogoNetFlix.png";
+import Link from "next/link";
+import Button from "../components/Button";
+import Input from "../components/Input";
 export default function Home() {
   return (
     <header className='header__main'>
@@ -8,14 +11,33 @@ export default function Home() {
         </div>
         <div className='header__nav'>
           <ul className='nav__list'>
-            <li className='list__item'>
-              <button>Languages</button>
+            <li className='nav__item'>
+            <Button type="button" classes="btn btn__color-black" title="Français">
+            </Button>
             </li>
-            <li className='list__item'>
-              <button>S'indentifier</button>
+            <li className='nav__item'>
+              <Link href="/login">
+                <Button type="button" classes="btn btn__color-red" title="S'identifier"></Button>
+              </Link>
             </li>
           </ul>
         </div>
+      </div>
+      <div className="header__below">
+        <h1>Films, séries TV et bien plus en illimité.</h1>
+        <h2>Où que vous soyez. Annulez à tout moment.</h2>
+        <form action="">
+          <h3>Prêt à regarder Netflix ? Saisissez votre adresse e-mail pour vous abonner ou réactiver votre abonnement.</h3>
+          <Input
+          name="email"
+          id="email"
+          type="email"
+          classes="form__input"
+          required={true}
+          placeholder="Adresse e-mail"
+          ></Input>
+          <Button type="button" classes="btn btn__color-red" title="Commencer >"></Button>
+        </form>
       </div>
       <div></div>
     </header>
