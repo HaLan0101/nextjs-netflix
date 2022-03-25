4e910b6dd0b122663c71e01718b3da4c6c04aada
HaLan0101 <dnhalan0101@gmail.com>
2022-03-25 03:39:24 +0100

    save code

---
+++ src/components/FilmCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Filmcard = () => {
+    return (
+        <>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+            <div className='film__card'>
+                <h2>Far from home</h2>
+                <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            </div>
+        </>
+    );
+}
+
+export default Filmcard;
--- src/pages/film/index.js
+++ src/pages/film/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FilmCard from "../../components/FilmCard";
+import filmService from "../../services/film.service";
 const Index = () => {
+    const [films, setFilms] = useState();
+    useEffect(() => {
+        filmService.getTrending()
+          .then((data) => {
+            console.log(data);
+            setFilms(data);
+          })
+        .catch(err=>console.log(err))
+      },[]);
     return (
         <>
             <Header></Header>
             <body>
                 <Hero></Hero>
                 <div className='film__list'>
-                    <div className='film__new'>
-                        <h1>Nouveautés</h1>
-                        <img src="" alt="" />
+                    <div className='film__trend'>
+                        <h1>Tendances actuelles</h1>
+                        <div className="film__grid">
+                            <FilmCard></FilmCard>
+                        </div>
                     </div>
                     <div className='film__genre'>
                         <h1>Actions</h1>
-                        <img src="" alt="" />
+                        <div className="film__grid"></div>
                     </div>
                     <div className='film__mylist'>
                         <h1>Ma list</h1>
-                        <img src="" alt="" />
+                        <div className="film__grid"></div>
                     </div>
                 </div>
                 
