1da7fe9685071c6b1a2b3a8289d7c0db7593e8f8
HaLan0101 <dnhalan0101@gmail.com>
2022-03-28 02:31:35 +0200

    save code

--- src/components/List.js
+++ src/components/List.js
@@ -38,22 +38,10 @@ const List = (props) => {
                 <div className="wrapper">
                 <FontAwesomeIcon icon="fa-solid fa-chevron-left" className="sliderArrow left" onClick={() => handleClick("left")} style={{ display: !isMoved && "none" }}/>
                     <div className="container" ref={listRef}>
-                    <ListItem index={0} />
-                    <ListItem index={1} />
-                    <ListItem index={2} />
-                    <ListItem index={3} />
-                    <ListItem index={4} />
-                    <ListItem index={5} />
-                    <ListItem index={6} />
-                    <ListItem index={7} />
-                    <ListItem index={8} />
-                    <ListItem index={9} />
-                    <ListItem index={10} />
-                    <ListItem index={11} />
-                    <ListItem index={12} />
-                    <ListItem index={13} />
-                    <ListItem index={14} />
-                    <ListItem index={15} />
+                    {films &&
+                        films.map((film,i) => (
+                            <ListItem index={i} film={film} key={film.id}></ListItem>
+                    ))}
                     </div>
                 <FontAwesomeIcon icon="fa-solid fa-chevron-right" className="sliderArrow right" onClick={() => handleClick("right")} />
                 </div>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const Listitem = ({index}) => {
+const Listitem = ({index,film}) => {
     const [isHovered, setIsHovered] = useState(false);
     const trailer="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
     return (
         <div className='listItem' style={{ left: isHovered && index * 225 - 50 + index * 2.5 }} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-            <img src="https://www.geekzone.fr/wp-content/uploads/2019/08/farfromhome_06.jpg" alt="" />
+            <img src={`http://image.tmdb.org/t/p/original${film.backdrop_path}`} alt="" />
             {isHovered && (
             <>
             <video src={trailer} autoPlay={true} loop />
@@ -16,14 +16,14 @@ const Listitem = ({index}) => {
                     <FontAwesomeIcon className="icon" icon="fa-solid fa-circle-plus" />
                     <FontAwesomeIcon className="icon" icon="fa-solid fa-thumbs-up" />
                 </div>
+                <div className='itemTitle'>{film.title}</div>
                 <div className="itemInfoTop">
                     <span className='itemInfoRecommand'>Recommandé à 98%</span>
                     <span className="limit">+16</span>
                     <span>1 h 54 min</span>
                 </div>
                 <div className="desc">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Praesentium hic rem eveniet error possimus, neque ex doloribus.
+                {film.overview.substring(0, 130)}...
                 </div>
                 <div className="genre">Action</div>
             </div>
